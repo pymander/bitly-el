d438f5f0d43fe57a8f73a001bca383cd022f982c
Erik L. Arneson <earneson@arnesonium.com>
2020-04-17 12:38:14 -0700

    Original bitly-el from ELPA

---
+++ bitly-autoloads.el
@@ -0,0 +1,23 @@
+;;; bitly-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "bitly" "bitly.el" (22110 9146 578141 110000))
+;;; Generated autoloads from bitly.el
+
+(autoload 'bitly-url-at-point "bitly" "\
+Replace the URL at point with a shortened one.
+
+With an active region, use the region contents as an URL.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; bitly-autoloads.el ends here
---
+++ bitly-pkg.el
@@ -0,0 +1,1 @@
+(define-package "bitly" "20151125.848" "Shorten URLs using the bitly.com shortener service" 'nil :url "https://github.com/jorgenschaefer/bitly-el")
